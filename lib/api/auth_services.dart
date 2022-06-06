01e1e4647804fc2624fae7f81f06346055f04e10
Tanti Wulansari <tantiwulansari2603@gmail.com>
2022-06-06 14:47:56 +0100

    commit

---
+++ lib/api/auth_services.dart
@@ -0,0 +1,39 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:go_job/api/api_services.dart';
+
+
+class AuthServices{
+  static Future<http.Response> register(String name, String email, String password) async {
+    Map data = {
+      "nama_pelamar":name,
+      "email":email,
+      "password":password,
+    };
+    var body = json.encode(data);
+    var url = Uri.parse(baseURL+'/register');
+    http.Response response = await http.post(
+      url,
+      headers: headers,
+      body: body
+    );
+    print(response.body);
+    return response; 
+  }
+
+  static Future<http.Response> login(String email, String password) async {
+    Map data = {
+      "email":email,
+      "password":password,
+    };
+    var body = json.encode(data);
+    var url = Uri.parse(baseURL+'/login');
+    http.Response response = await http.post(
+      url,
+      headers: headers,
+      body: body
+    );
+    print(response.body);
+    return response; 
+  }
+}
